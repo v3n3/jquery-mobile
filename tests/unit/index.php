14f560142aedf3c7b04ca3d28b88160eb826e5e5
v3n3 <julien.veneziano@gmail.com>
2011-08-12 12:50:32 -0400

    update repo with beta2 data
    Change-Id: Ifa5fa24efc27cddf6e0bd15d06d2ee7e1416ec4d

--- tests/unit/index.php
+++ tests/unit/index.php
@@ -3,6 +3,9 @@
 <head>
   <meta charset="utf-8">
 	<link rel="stylesheet" href="../../external/qunit.css"></link>
+	<script src="../../js/jquery.js"></script>
+	<script src="../../external/qunit.js"></script>
+	<script src="runner.js"></script>
 </head>
 <body>
   <div style="float: left; width: 500px;">
@@ -15,8 +18,5 @@
        I think an entire link and stylesheet is a waste -->
 	<iframe data-src="../../tests/unit/{{testdir}}/index.html" name="testFrame" id="testFrame" width="800px" height="100%" style="float: left; border: 0px; height: 100%; ">
 	</iframe>
-	<script src="../../js/jquery.js"></script>
-	<script src="../../external/qunit.js"></script>
-	<script src="runner.js"></script>
 </body>
 </html>
